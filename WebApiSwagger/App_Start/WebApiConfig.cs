87c46fbc7d4f8685f270614cafec44d1bda24d00
iabuhilal <ihab_abu_hilal@hotmail.com>
2016-11-18 11:32:13 -0500

    add rate limit

--- WebApiSwagger/App_Start/WebApiConfig.cs
+++ WebApiSwagger/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebApiThrottle;
 
 namespace WebApiSwagger
 {
@@ -9,6 +10,21 @@ namespace WebApiSwagger
     {
         public static void Register(HttpConfiguration config)
         {
+
+          // https://github.com/stefanprodan/WebApiThrottle#global-throttling-based-on-ip
+          //The setup bellow will limit the number of requests originated from the same IP. 
+          //If from the same IP, in same second, you'll make a call to api/values and api/values/1 
+          //the last call will get blocked.
+
+          config.MessageHandlers.Add(new ThrottlingHandler()
+          {
+            Policy = new ThrottlePolicy(perSecond: 1, perMinute: 20, perHour: 200, perDay: 1500, perWeek: 3000)
+            {
+              IpThrottling = true
+            },
+            Repository = new CacheRepository()
+          });
+
             // Web API configuration and services
 
             // Web API routes
@@ -19,6 +35,8 @@ namespace WebApiSwagger
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+
         }
     }
 }
